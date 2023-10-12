6f770a35c0bbe06243a36de548a5f46a0c2f4d9b
MRajczyk <mikolajrajczyk01@gmail.com>
2023-10-12 23:43:13 +0200

    first commit

---
+++ firebase.js
@@ -0,0 +1,30 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/compat/app";
+import 'firebase/compat/auth';
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.EXPO_PUBLIC_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+let app;
+if(firebase.apps.length === 0) {
+  app = firebase.initializeApp(firebaseConfig);
+} else {
+  app = firebase.app();
+}
+const analytics = getAnalytics(app);
+
+const auth = firebase.auth();
+export { auth };
