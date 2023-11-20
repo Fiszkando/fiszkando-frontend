3c7614660e239dd5f8356c31643bc838b9a1c55e
MRajczyk <mikolajrajczyk01@gmail.com>
2023-11-20 19:10:53 +0100

    cleanup

--- screens/AddNewScreen.jsx
+++ screens/AddNewScreen.jsx
@@ -11,15 +11,10 @@ import {
   Alert,
 } from "react-native";
 import React, { useEffect, useState } from "react";
-import { auth } from "../firebase";
-import { signOut } from "firebase/auth";
 import { Dimensions } from "react-native";
-import { updatePassword, updateProfile, deleteUser } from "firebase/auth";
 
 const { height } = Dimensions.get("window");
 const backgroundImg = require("../assets/tlo.png");
-const saveIcon = require("../assets/diskette.png");
-const deleteIcon = require("../assets/bin.png");
 
 const ProfileScreen = () => {
   return (
@@ -98,90 +93,5 @@ const styles = StyleSheet.create({
   titleText: {
     fontFamily: "Harlow-Solid-Italic",
     fontSize: 40,
-  },
-  cardsScrollviewContainer: {
-    marginTop: 0.15 * height,
-    height: "100%",
-    flex: 1,
-  },
-  cardsContainer: {
-    // backgroundColor: 'yellow',
-  },
-  cardsContainerContent: {
-    gap: 20,
-    // backgroundColor: 'red',
-    alignItems: "center",
-    paddingBottom: 20,
-  },
-  cardContainer: {
-    width: "100%",
-    height: 0.32 * height,
-    alignItems: "center",
-  },
-  cardBackground: {
-    width: "80%",
-    backgroundColor: "white",
-    borderRadius: 20,
-    top: 0.04 * height,
-    shadowColor: "black",
-    shadowOffset: {
-      width: 0,
-      height: 10,
-    },
-    shadowOpacity: 0.5,
-    shadowRadius: 12,
-    elevation: 12,
-  },
-  cardIconBackground: {
-    width: "18%",
-    height: "28%",
-    backgroundColor: "white",
-    borderRadius: 100,
-    top: "-12%",
-    left: "5%",
-    justifyContent: "center",
-    alignItems: "center",
-    shadowColor: "black",
-    shadowOffset: {
-      width: 0,
-      height: 10,
-    },
-    shadowOpacity: 0.5,
-    shadowRadius: 12,
-    elevation: 20,
-  },
-  cardTitle: {
-    fontSize: 20,
-    color: "#2F93BE",
-    fontWeight: "bold",
-    top: -10,
-    left: 35,
-  },
-  cardText: {
-    color: "black",
-    paddingHorizontal: 30,
-    marginBottom: 10,
-  },
-  buttonContainer: {
-    alignItems: "center",
-    width: "70%",
-  },
-  button: {
-    backgroundColor: "#2F93BE",
-    width: "100%",
-    padding: 15,
-    borderRadius: 33,
-    alignItems: "center",
-  },
-  buttonOutline: {
-    backgroundColor: "white",
-    marginTop: 5,
-    borderColor: "#2F93BE",
-    borderWidth: 1,
-  },
-  buttonText: {
-    color: "black",
-    fontWeight: "700",
-    fontSize: 16,
   },
 });
